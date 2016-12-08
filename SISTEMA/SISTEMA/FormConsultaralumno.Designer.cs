6fc5cacfbe86e13c3ddd299e96f1294b35b3ddd6
julioeck <julio_pacto@hotmail.com>
2016-12-07 22:11:20 -0700

    a

--- SISTEMA/SISTEMA/FormConsultaralumno.Designer.cs
+++ SISTEMA/SISTEMA/FormConsultaralumno.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.label1 = new System.Windows.Forms.Label();
             this.btn_regresar = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -51,11 +52,21 @@
             this.btn_regresar.UseVisualStyleBackColor = true;
             this.btn_regresar.Click += new System.EventHandler(this.button1_Click);
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(155, 55);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // FormConsultaralumno
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.btn_regresar);
             this.Controls.Add(this.label1);
             this.Name = "FormConsultaralumno";
@@ -69,5 +80,6 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button btn_regresar;
+        private System.Windows.Forms.Button button1;
     }
 }
